b4da1933e3a22b91c55afaed6b7bdc2514be22fd
Krasimir D <detha4545@gmail.com>
2021-04-17 21:14:49 +0300

    Add homeworld filtering

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const BASE_URL = "https://swapi.dev/api";
-let allPpl = [];
+let allItems = [];
 function App() {
   let [items, setItems] = useState([]);
   let [isLoading, setIsLoading] = useState(false);
 
   useEffect(async () => {
     await getAllItems(`${BASE_URL}/people/`);
-    await getAllItems(`${BASE_URL}/planets/`);
-    await getAllItems(`${BASE_URL}/films/`);
     await getAllItems(`${BASE_URL}/species/`);
+    await getAllItems(`${BASE_URL}/planets/`);
     await getAllItems(`${BASE_URL}/starships/`);
     await getAllItems(`${BASE_URL}/vehicles/`);
-    setItems(allPpl);
+    setItems(allItems);
     setIsLoading(false);
   }, []);
 
@@ -21,9 +20,13 @@ function App() {
     setIsLoading(true);
     let res = await (await fetch(url)).json();
 
-    res.results.forEach((x) => {
+    res.results.forEach(async (x) => {
       if (x.name) {
-        allPpl.push(x);
+        if (x.homeworld) {
+          let obj = await getSingleItem(x.homeworld);
+          x.homeWorld = obj.name;
+        }
+        allItems.push(x);
       }
     });
 
@@ -32,25 +35,71 @@ function App() {
     }
   }
 
+  async function getSingleItem(url) {
+    return await (await fetch(url)).json();
+  }
+
   function onChangeSearchHandler(e) {
+    let filter = document.querySelector('#filter').value;
     let value = e.target.value;
-    let filteredPpl = allPpl.filter(
-      (x) => x.name && x.name.toLowerCase().includes(value.toLowerCase())
-    );
-    setItems(filteredPpl);
-    console.log(allPpl);
+    let filteredItems;
+    if (filter == 'name') {
+      filteredItems = allItems.filter(
+        (x) => x.name && x.name.toLowerCase().includes(value.toLowerCase())
+      );
+    } else if (filter) {
+      filteredItems = allItems.filter(
+        (x) => x.homeWorld && x.homeWorld.toLowerCase().includes(value.toLowerCase())
+      );
+    }
+    
+    window.scrollTo(0, 0);
+    setItems(filteredItems);
   }
 
   return (
-    <div className="bg-white">
-      <label>Search</label>
-      <input
-        onChange={onChangeSearchHandler}
-        placeholder="Darth Vader..."
-      ></input>
-      {isLoading && <h1>Loading...</h1>}
-      <ul>{!isLoading && items.map((x) => <li key={x.name}>{x.name}</li>)}</ul>
-    </div>
+    <React.Fragment>
+      {isLoading ? (
+        <h1 className="blue-text position-absolute top-50 start-50 translate-middle mt-1 bi bi-caret-down-fill">
+          Loading...
+        </h1>
+      ) : (
+        <div className="container">
+          <div className="row">
+            <div className="col-md-9">
+              <input
+                className="sticky-top form-control bg-dark blue-text border border-primary"
+                onChange={onChangeSearchHandler}
+              ></input>
+            </div>
+            <div className="col-md-3">
+              <select
+                id="filter"
+                className="form-select blue-text bg-dark border border-primary"
+                aria-label="Default select example"
+              >
+                <option className="blue-text" value="name" selected>Name</option>
+                <option className="blue-text" value="homeworld">Homeworld</option>
+              </select>
+            </div>
+          </div>
+
+          <ul className="list-group border border-dark">
+            {!isLoading &&
+              items.map((x) => (
+                <li key={x.name} className="bg-dark blue-text list-group-item">
+                  <div>
+                    <h4>{x.name}</h4>
+                    <h4 className="white-text">
+                      {`${x.homeWorld == "unknown" || !x.homeWorld ? "Unknown" : x.homeWorld}`}
+                    </h4>
+                  </div>
+                </li>
+              ))}
+          </ul>
+        </div>
+      )}
+    </React.Fragment>
   );
 }
 
